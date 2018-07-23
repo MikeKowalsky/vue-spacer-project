55bc0871201a08cd2cef6128c9b9e52090e34c0c
Michal Kowalewski <mike@Michals-MacBook-Pro.local>
2018-07-23 22:19:15 +0200

    Finished app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,79 @@
 <template>
-  <div class="app">
-    <!-- <div class="nav">
-      <router-link to="/">spacer</router-link>
-      <router-link to="/about">about</router-link>
-    </div> -->
-    <router-view />
+  <div :class="[{ flexStart: step === 1 }, 'wrapper']">
+    <transition name="slide">
+      <img src="./assets/logo.svg" alt="logo" class="logo" v-if="step === 1">
+    </transition>
+    <transition name="fade">
+      <HeroImage v-if="step === 0"/>
+    </transition>
+    <Claim v-if="step === 0"/>
+    <SearchInput v-model="searchValue" @input="handleInput" :dark="step === 1"/>
+    <div class="results" v-if="results && !loading && step === 1">
+      <Item
+        v-for="item in results"
+        :item="item"
+        :key="item.data[0].nasa_id"
+        @click.native="handleModalOpen(item)" />
+    </div>
+    <div class="loader" v-if="step === 1 && loading" />
+    <Modal
+      v-if="modalOpen"
+      @closeModal="modalOpen = false"
+      :item="modalItem" />
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+import debounce from 'lodash.debounce';
+import Claim from '@/components/Claim.vue';
+import SearchInput from '@/components/SearchInput.vue';
+import HeroImage from '@/components/HeroImage.vue';
+import Item from '@/components/Item.vue';
+import Modal from '@/components/Modal.vue';
+
+const API = 'https://images-api.nasa.gov/search';
+
+export default {
+  name: 'App',
+  components: {
+    Claim,
+    SearchInput,
+    HeroImage,
+    Item,
+    Modal,
+  },
+  data() {
+    return {
+      modalOpen: false,
+      modalItem: null,
+      loading: false,
+      step: 0,
+      searchValue: '',
+      results: [],
+    };
+  },
+  methods: {
+    handleModalOpen(item) {
+      this.modalOpen = true;
+      this.modalItem = item;
+    },
+    handleInput: debounce(function () {
+      this.loading = true;
+      axios.get(`${API}?q=${this.searchValue}&media_type=image`)
+        .then((response) => {
+          this.results = response.data.collection.items;
+          this.loading = false;
+          this.step = 1;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 500),
+  },
+};
+</script>
+
 
 <style lang="scss">
 
@@ -14,6 +81,8 @@
 
   *{
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   body{
@@ -21,4 +90,89 @@
     margin: 0px;
     padding: 0px;
   }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .3s ease;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+
+  .slide-enter-active, .slide-leave-active {
+    transition: margin-top .3s ease;
+  }
+  .slide-enter, .slide-leave-to {
+    margin-top: -50px;
+  }
+
+  .wrapper {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    min-height: 100vh;
+    margin: 0;
+    padding: 30px;
+
+    &.flexStart {
+      justify-content: flex-start;
+    }
+  }
+
+  .loader {
+    margin-top: 100px;
+    display: inline-block;
+    width: 64px;
+    height: 64px;
+
+    @media (min-width: 768px) {
+      width: 90px;
+      height: 90px;
+    }
+  }
+
+  .loader:after {
+    content: " ";
+    display: block;
+    width: 46px;
+    height: 46px;
+    margin: 1px;
+    border-radius: 50%;
+    border: 5px solid #1e3d4a;
+    border-color: #1e3d4a transparent #1e3d4a transparent;
+    animation: loading 1.2s linear infinite;
+
+    @media (min-width: 768px) {
+      width: 90px;
+      height: 90px;
+    }
+  }
+
+  @keyframes loading {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  .logo {
+    position: absolute;
+    top: 30px;
+  }
+
+  .results {
+    margin-top: 50px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+
+    @media (min-width: 768px) {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+  }
+
 </style>
--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,33 +1,71 @@
 <template>
-    <div class="searchWrapper">
-        <input
-        id="search"
-        name="search"
-        v-model="searchValue"
-        @input="handleInput"
-        />
-    </div>
+    <input
+    id="search"
+    name="search"
+    :class="{ dark: dark}"
+    :value="value"
+    @input="handleChange"
+    />
 </template>
+
+// :class (first class name: than condition)
+// can be also :class (dark: a > 1)
+// due to es6 can be :class (dark) because both have the same name
 
 <script>
 export default {
   name: 'SearchInput',
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    dark: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    handleChange(e) {
+      this.$emit('input', e.target.value);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-  .searchWrapper {
+  input {
     margin-top: 50px;
     display: flex;
-    flex-direction: column;
-    width: 300px;
-  }
+    width: 250px;
 
-  input {
+    color: white;
+    text-align: center;
+    font-size: 18px;
+    font-weight: 300;
     height: 30px;
     border: 0;
     background: none;
-    border-bottom: 1px solid black;
+    border-bottom: 1px solid white;
+    transition: box-shadow .3s ease-out;
+
+    @media (min-width: 1024px){
+        font-weight: 400;
+    }
+  }
+
+  input:focus{
+      outline: none;
+      box-shadow: 0 10px 20px -8px rgba(255,255,255, .5);
+  }
+
+  .dark {
+      color: #1e3d4a;
+      border-bottom-color: #1e3d4a;
+  }
+
+  .dark:focus{
+      box-shadow: 0 10px 20px -8px rgba(#1e3d4a, .2);
   }
 
 </style>
